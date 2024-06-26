28d423f19437dba3b857254377e1efebc7d45a81
Nandana Narayan Das <97794941+nandana-das@users.noreply.github.com>
2024-06-26 20:44:22 +0530

    Update LRUCacheTest.java

--- src/main/java/LRUCacheTest.java
+++ src/main/java/LRUCacheTest.java
@@ -15,20 +15,30 @@ public class LRUCacheTest {
         options.maxOpenFiles(256);
         DB db = Iq80DBFactory.factory.open(new File("testdb"), options);
 
+        CacheCalculator cacheCalculator = new CacheCalculator(options);
+        
         // Insert some data into the database
-        for (int i = 0; i < 1000; i++) {
-            db.put(("key" + i).getBytes(), ("value" + i).getBytes());
+        for (int i = 0; i < 100; i++) {
+            byte[] key = ("key" + i).getBytes();
+            byte[] value = ("value" + i).getBytes();
+            db.put(key, value);
+            cacheCalculator.updateCacheSize(key.length + value.length);
         }
 
         // Get some data from the database to test the cache
         long startTime = System.currentTimeMillis() / 1000; // epoch time in seconds
         for (int i = 0; i < 100; i++) {
-            byte[] value = db.get(("key" + i).getBytes());
+            byte[] key = ("key" + i).getBytes();
+            byte[] value = db.get(key);
             System.out.println("Value for key" + i + ": " + new String(value));
+            cacheCalculator.updateCacheSize(-key.length - value.length); // subtract the size of the retrieved data
         }
+        
         long endTime = System.currentTimeMillis() / 1000; // epoch time in seconds
         System.out.println("Time taken: " + (endTime - startTime) + " seconds");
 
+        System.out.println("Remaining cache size: " + cacheCalculator.getRemainingCacheSize() + " bytes");
+ 
         // Access the first value in the cache
         DBIterator iterator = db.iterator();
         iterator.seekToFirst();
@@ -38,6 +48,38 @@ public class LRUCacheTest {
             System.out.println("First key: " + new String(firstKey));
             System.out.println("First value: " + new String(firstValue));
         }
+// Insert some data into the database
+        for (int i = 100; i < 200; i++) {
+            byte[] key = ("key" + i).getBytes();
+            byte[] value = ("value" + i).getBytes();
+            db.put(key, value);
+            cacheCalculator.updateCacheSize(key.length + value.length);
+        }
+
+        // Get some data from the database to test the cache
+        startTime = System.currentTimeMillis() / 1000; // epoch time in seconds
+        for (int i = 0; i < 200; i++) {
+            byte[] key = ("key" + i).getBytes();
+            byte[] value = db.get(key);
+            System.out.println("Value for key" + i + ": " + new String(value));
+            cacheCalculator.updateCacheSize(-key.length - value.length); // subtract the size of the retrieved data
+        }
+        endTime = System.currentTimeMillis() / 1000; // epoch time in seconds
+        System.out.println("Time taken: " + (endTime - startTime) + " seconds");
+
+        System.out.println("Remaining cache size: " + cacheCalculator.getRemainingCacheSize() + " bytes");
+
+        // Access the first value in the cache again
+        iterator = db.iterator();
+        iterator.seekToFirst();
+        if (iterator.hasNext()) {
+            byte[] firstKey = iterator.next().getKey();
+            byte[] firstValue = iterator.peekNext().getValue();
+            System.out.println("First key: " + new String(firstKey));
+            System.out.println("First value: " + new String(firstValue));
+        }
+
+        
         iterator.close();
 
         // Close the database
